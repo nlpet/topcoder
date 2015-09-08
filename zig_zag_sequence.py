4a36487a9a00a93a609eb79d559839d26894b0b8
Nora Petrova <schottkey@gmail.com>
2015-09-08 17:37:36 +0100

    Update zig_zag_sequence.py

--- zig_zag_sequence.py
+++ zig_zag_sequence.py
@@ -10,23 +10,33 @@ class TestZigZag(unittest.TestCase):
 
     def tests(self):
         """Tests."""
-        self.assertEqual(longest_zig_zag_sequence([1, 7, 4, 9, 2, 5]), 6)
-        self.assertEqual(longest_zig_zag_sequence([1, 17, 5, 10, 13, 15, 10, 5, 16, 8]), 7)
-        self.assertEqual(longest_zig_zag_sequence([1, 2, 3, 4, 5, 6, 7, 8, 9]), 2)
+        self.assertEqual(longest_zig_zag_seq([1, 7, 4, 9, 2, 5]), 6)
+        self.assertEqual(longest_zig_zag_seq([1, 17, 5, 10, 13, 15, 10, 5, 16, 8]), 7)
+        self.assertEqual(longest_zig_zag_seq([1, 2, 3, 4, 5, 6, 7, 8, 9]), 2)
 
-def longest_zig_zag_sequence(seq):
+def longest_zig_zag_seq(seq):
     """
     :list : seq
     """
     n = len(seq)
-    if n == 1:
-        return 1
-    deltas = [0 for _ in range(n - 1)]
+    best_length = 1
+    upwards, downwards = [1], [1]
     for i in range(1, n):
-        deltas[i - 1] = seq[i] - seq[i - 1]
-    return deltas
+        upwards.append(1)
+        downwards.append(1)
+
+        for j in range(i):
+            if seq[i] > seq[j]:
+                upwards[i] = max(downwards[j] + 1, upwards[i])
+            if seq[i] < seq[j]:
+                downwards[i] = max(upwards[j] + 1, downwards[i])
+
+        best_length = max(best_length, max(upwards[i], downwards[i]))
+    return best_length
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    print longest_zig_zag_sequence([1, 17, 5, 10, 13, 15, 10, 5, 16, 8])
+    unittest.main()
+    # subseq = [1, 17, 5, 10, 13, 15, 10, 5, 16, 8]
+    # print subseq
+    # print longest_zig_zag_seq(subseq)
